8106e86fcd4d6e949b3845bb7df9d16746cbefcb
Kirill <hrothnar@mail.ru>
2022-12-08 11:22:30 +0600

    Homework was done

--- src/main/java/my/app/cookbook/controllers/FirstController.java
+++ src/main/java/my/app/cookbook/controllers/FirstController.java
@@ -1,2 +1,19 @@
-package my.app.cookbook.controllers;public class FirstController {
+package my.app.cookbook.controllers;
+
+import my.app.cookbook.CookBookApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+    @GetMapping("/")
+    public String primal() {
+        return "Приложение запущено";
+    }
+    @GetMapping("/info")
+    public String info() {
+        return "Имя: Кирилл Название проекта: CookBook Дата создания: 08.12.2022 Описание: Проект является поваренной книгой, которая содержит вкусные рецепты, для приготовления блюд";
+    }
+
+
 }
